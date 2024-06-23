ea690dbd536fb01693310425e9f44180940c4391
Antonio Mikael <antoniomikael500@gmail.com>
2024-06-23 17:05:46 -0300

    adicionando tarefa de forma dinamica

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-$(document).ready(()=> {
-   
-    $('header form button').click((e)=>{
-        e.preventDefault();
-        const inputTarefa = $('#input-tarefa').val();
-        console.log(inputTarefa);
-    })
+$(document).ready(() => {
+   const tarefas = [];
+
+   $('form').on('submit', (e) => {
+      e.preventDefault();
+
+      let tarefaNome = $('#input-tarefa').val();
+      let feito = $('#feito').is(':checked');
+
+      
+      if (tarefas.includes(tarefaNome.value)) {
+         alert('Tarefa ja inserida');
+      }
+      else {
+         let tarefaItem = $('<li>').text(tarefaNome);
+         let statusSpan = $('<span>').text(feito ? '- ✔️' : '- ❌');
+         if (feito) {
+            tarefaItem.addClass('feito');
+         }
+         else {
+            tarefaItem.addClass('nao-feito');
+         }
+         tarefaItem.append(' ').append(statusSpan);
+
+         $('#lista-tarefas').append(tarefaItem).append('<hr>');
+
+         $('#input-tarefa').val('');
+         $('#feito').prop('checked', false);
+         tarefas.push(tarefaNome.value);
+      }
+
+   })
+
 })
